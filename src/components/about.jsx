5e2fdddce08bfd751043e28186b8eb44352783d7
Sofrito4TheWorld <jaime.gonzalez5@upr.edu>
2020-07-31 14:51:48 -0400

    Updating Mess

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,8 +13,8 @@ export default class About extends Component {
                     <div className="about-desc">
                     <span className="heading-meta">About Us</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                    <p>I am a computer engineer from University of Puerto Rico Mayaguez campus.I love exploring new technologies and am often amazed by the progress we as a human species have mad so far in the recent years. My goal is to contribute to said progress with my grain of sand.</p>
+                    <p> </p>
                     </div>
                 </div>
                 </div>
@@ -37,8 +37,8 @@ export default class About extends Component {
                     <i className="icon-bulb" />
                 </span>
                 <div className="desc">
-                    <h3>Web Development </h3>
-                    <p>I have experience building websites and chrome extentions using JavaScript,React,HTML,CSS</p>
+                    <h3>Machine Learning </h3>
+                    <p>I have experience in data analysis and building predictive models using Python's Scikit-learn, Tensorflow and Keras</p>
                 </div>
                 </div>
             </div>
@@ -49,7 +49,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Data Structures & Algorithms</h3>
-                    <p>As coming from the CS background, I have good grasp over fundamental concepts of DSA</p>
+                    <p>As coming from a computer engineering background , I have good grasp over fundamental concepts of DSA</p>
                 </div>
                 </div>
             </div>
@@ -59,8 +59,8 @@ export default class About extends Component {
                     <i className="icon-data" />
                 </span>
                 <div className="desc">
-                    <h3>Dev Ops</h3>
-                    <p>I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
+                    <h3>Automation</h3>
+                    <p>Experienced scripting in Python and using Jenkins</p>
                 </div>
                 </div>
             </div>
