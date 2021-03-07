43db6c42f9f67aa1a9c8adfea536f026de347c46
Sofrito4TheWorld <jaime.gonzalez5@upr.edu>
2021-03-06 21:59:29 -0400

    Updates

---
+++ precache-manifest.47ea55226f8b77b6d983f8bacf9630b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9374cbd4494f36cd42d47e7300bc96b7",
+    "url": "/gh-pages/index.html"
+  },
+  {
+    "revision": "87fd359611d50d331b62",
+    "url": "/gh-pages/static/css/main.6d7aa358.chunk.css"
+  },
+  {
+    "revision": "f0ff664296d7125776ce",
+    "url": "/gh-pages/static/js/2.9e10a483.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gh-pages/static/js/2.9e10a483.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87fd359611d50d331b62",
+    "url": "/gh-pages/static/js/main.3c956d9e.chunk.js"
+  },
+  {
+    "revision": "2ea3e6bd1d1df1b3592f",
+    "url": "/gh-pages/static/js/runtime-main.5b6f9241.js"
+  }
+]);
